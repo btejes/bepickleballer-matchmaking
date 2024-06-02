3041ff7e76937f0aaee82a8fa75cdb00a7d08492
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-02 09:01:02 -0700

    Updated Email and Phone fields for matches modal

--- app/matches/Modal.jsx
+++ app/matches/Modal.jsx
@@ -64,22 +64,24 @@ const Modal = ({ match, onClose, onUnmatch }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-75">
-      <div className="bg-white rounded-lg p-4 w-96 relative">
+      <div className="bg-white rounded-3xl p-4 w-96 relative">
         <button className="absolute top-2 right-2 text-gray-500" onClick={onClose}>
           ×
         </button>
         {!showRating ? (
           <>
             <ProfileCard profile={match} />
-            <p className="mt-2">Email: {match.email}</p>
-            <p>Phone: {match.phone}</p>
-            <div className="flex justify-between mt-4">
-              <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={handleUnmatchClick}>
-                Unmatch
-              </button>
-              <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={handleRateClick}>
-                Rate
-              </button>
+            <div className="px-4 text-center">
+              <p className="mt-2 text-sm"><strong>Phone:</strong> {match.phone}</p>
+              <p className="mt-1 text-sm"><strong>Email:</strong> {match.email}</p>
+              <div className="flex justify-between mt-4 space-x-4">
+                <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={handleUnmatchClick}>
+                  Unmatch
+                </button>
+                <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={handleRateClick}>
+                  Rate
+                </button>
+              </div>
             </div>
           </>
         ) : (
