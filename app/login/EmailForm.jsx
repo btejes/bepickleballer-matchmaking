83afb8b4a579e0d3f73da016f08d5ad507becf9b
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-02 17:46:15 -0700

    Updated login route for new login limiter

--- app/login/EmailForm.jsx
+++ app/login/EmailForm.jsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const EmailForm = () => {
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(900); // 15 minutes countdown
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,36 +20,62 @@ const EmailForm = () => {
       });
 
       if (response.ok) {
-        setMessage("Check your email for the secure login link.");
+        setEmailSent(true);
       } else {
-        setMessage("Error sending login link.");
+        console.error("Error sending login link.");
       }
     } catch (error) {
-      setMessage("Error sending login link.");
+      console.error("Error sending login link.");
     }
   };
 
+  useEffect(() => {
+    if (emailSent && timeLeft > 0) {
+      const timer = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  }, [emailSent, timeLeft]);
+
+
+
   return (
     <div>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <div className="mb-4 w-full">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value.toLowerCase())}
-            placeholder="Enter your email"
-            required
-            className="input input-bordered w-full border border-gray-400 focus:border-blue focus:ring-2 focus:ring-blue text-black bg-white rounded-lg shadow-sm px-3 text-center"
-          />
+      {emailSent ? (
+        <div className="text-center">
+          <h2 className="text-3xl font-bold mb-4 text-center text-darkGray">Check Email For Your Secure Magic Link!</h2>
+          <p className="mt-4 text-center text-black">
+            A secure login link, expiring in 15 minutes, has been sent to <strong>{email}</strong> Click the link in the email to log in to your account.
+          </p>
         </div>
-        <button
-          type="submit"
-          className="btn bg-blue-500 text-white px-4 py-2 hover:bg-blue-700 rounded-lg shadow-sm"
-        >
-          Get Login Link
-        </button>
-      </form>
-      {message && <p className="mt-4 text-center text-black">{message}</p>}
+      ) : (
+        <>
+          <h2 className="text-3xl font-bold mb-4 text-center text-darkGray">Login</h2>
+          <p className="mb-6 text-center text-darkGray">
+            Enter your email to get a secure login link. This magic link lets you sign in/up without needing a password.
+          </p>
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <div className="mb-4 w-full">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value.toLowerCase())}
+                placeholder="Enter your email"
+                required
+                className="input input-bordered w-full border border-gray-400 focus:border-blue focus:ring-2 focus:ring-blue text-black bg-white rounded-lg shadow-sm px-3 text-center"
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn bg-blue-500 text-white px-4 py-2 hover:bg-blue-700 rounded-lg shadow-sm"
+              disabled={emailSent}
+            >
+              Get Login Link
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,10 @@ import EmailForm from "./login/EmailForm";
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-lightGreen">
-      <h1 className="text-4xl font-bold mb-6 text-center text-black">
+      <h1 className="text-5xl font-bold mb-6 text-center text-black">
         Welcome to Be a Pickle Baller Matchmaking
       </h1>
       <div className="p-6 max-w-md w-full bg-white rounded-lg shadow-md mt-4">
-        <h2 className="text-3xl font-bold mb-4 text-center text-darkGray">Login</h2>
-        <p className="mb-6 text-center text-darkGray">
-          Enter your email to receive a secure login link. This will sign you into the matchmaking page.
-        </p>
         <EmailForm />
       </div>
     </div>
