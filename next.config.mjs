906edcd5234e1e2e34ed4bf5f0c9cbeb5472f020
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-05 18:57:09 -0700

    updates for subdirectory issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? '/matchmaking' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/matchmaking' : '',
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.alias['@sentry/node'] = '@sentry/browser';
+      }
+      return config;
+    },
   };
   
   export default nextConfig;
