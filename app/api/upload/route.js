ecea5b70df885a4d93c45ab99efea97fee3812be
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-09 23:36:32 -0700

    fix last build error issue hopefully

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,6 +1,7 @@
 import aws from 'aws-sdk';
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
 
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -10,27 +11,29 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
-export async function POST(req) {
-  const cookie = cookies();
-  const token = cookie.get('jwt');
+export const dynamic = 'force-dynamic';
 
-  if (!token) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
-
-  const formData = await req.formData();
-  const file = formData.get('file');
-  const userId = formData.get('userId');
-
-  const params = {
-    Bucket: process.env.S3_BUCKET_NAME,
-    Key: `${userId}/${file.name}`,
-    Body: file,
-    ContentType: file.type,
-    ACL: 'public-read',
-  };
-
+export async function POST(request) {
   try {
+    const jwtToken = cookies().get('token')?.value;
+    if (!jwtToken) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
+
+    const formData = await request.formData();
+    const file = formData.get('file');
+    const userId = decoded._id;
+
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: `${userId}/${file.name}`,
+      Body: file,
+      ContentType: file.type,
+      ACL: 'public-read',
+    };
+
     const data = await s3.upload(params).promise();
     return NextResponse.json({ Location: data.Location }, { status: 200 });
   } catch (error) {
@@ -38,9 +41,3 @@ export async function POST(req) {
     return NextResponse.json({ error: 'Error uploading to S3' }, { status: 500 });
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
