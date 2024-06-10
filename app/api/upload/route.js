8fa00074ca92e798f07923943c3ee44154e9cbc1
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-09 23:47:37 -0700

    fix build issues with api upload issue

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -2,6 +2,7 @@ import aws from 'aws-sdk';
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
+import formidable from 'formidable';
 
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -13,7 +14,7 @@ const s3 = new aws.S3();
 
 export const dynamic = 'force-dynamic';
 
-export async function POST(request) {
+export async function POST(req) {
   try {
     const jwtToken = cookies().get('token')?.value;
     if (!jwtToken) {
@@ -22,22 +23,34 @@ export async function POST(request) {
 
     const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
 
-    const formData = await request.formData();
-    const file = formData.get('file');
-    const userId = decoded._id;
+    const form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error('Error parsing form data:', err);
+        return NextResponse.json({ error: 'Error parsing form data' }, { status: 500 });
+      }
 
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: `${userId}/${file.name}`,
-      Body: file,
-      ContentType: file.type,
-      ACL: 'public-read',
-    };
+      const file = files.file;
+      const userId = decoded._id;
 
-    const data = await s3.upload(params).promise();
-    return NextResponse.json({ Location: data.Location }, { status: 200 });
+      const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `${userId}/${file.name}`,
+        Body: fs.createReadStream(file.path),
+        ContentType: file.type,
+        ACL: 'public-read',
+      };
+
+      try {
+        const data = await s3.upload(params).promise();
+        return NextResponse.json({ Location: data.Location }, { status: 200 });
+      } catch (error) {
+        console.error('Error uploading to S3:', error);
+        return NextResponse.json({ error: 'Error uploading to S3' }, { status: 500 });
+      }
+    });
   } catch (error) {
-    console.error('Error uploading to S3:', error);
-    return NextResponse.json({ error: 'Error uploading to S3' }, { status: 500 });
+    console.error('Error:', error);
+    return NextResponse.json({ error: 'Error' }, { status: 500 });
   }
 }
