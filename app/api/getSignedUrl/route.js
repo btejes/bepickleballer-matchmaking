d43c84c9b930b74e0df7c73a19eed43984eac8d5
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-10 18:12:22 -0700

    revert to non build failing file

--- app/api/getSignedUrl/route.js
+++ app/api/getSignedUrl/route.js
@@ -5,12 +5,12 @@ import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
 
 const s3Client = new S3Client({
-    region: process.env.AWS_BUCKET_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-  });
+  region: process.env.AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 console.log('AWS_ACCESS_KEY:', process.env.AWS_ACCESS_KEY);
 console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
@@ -18,15 +18,15 @@ console.log('AWS_BUCKET_NAME:', process.env.AWS_BUCKET_NAME);
 console.log('AWS_BUCKET_REGION:', process.env.AWS_BUCKET_REGION);
 
 export async function POST() {
-console.log('POST request received');
+  console.log('POST request received');
 
-try {
+  try {
     const jwtToken = cookies().get('token')?.value;
     console.log('JWT token:', jwtToken);
 
     if (!jwtToken) {
-    console.log('No JWT token found');
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+      console.log('No JWT token found');
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
     const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
@@ -34,21 +34,21 @@ try {
     console.log('Token verified. User ID:', userId);
 
     const putObjectCommand = new PutObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: `${userId}/${Date.now()}`,
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `${userId}/${Date.now()}`,
     });
 
     console.log('PutObjectCommand created:', putObjectCommand);
 
     const signedUrl = await getSignedUrl(s3Client, putObjectCommand, {
-    expiresIn: 60,
+      expiresIn: 60,
     });
 
     console.log('Signed URL generated:', signedUrl);
 
     return NextResponse.json({ url: signedUrl }, { status: 200 });
-} catch (error) {
+  } catch (error) {
     console.error('Error generating signed URL:', error);
     return NextResponse.json({ error: 'Failed to generate signed URL' }, { status: 500 });
-}
+  }
 }
