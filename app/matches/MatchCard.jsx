eeb986c2b94dc2e81cd3730977e4276f94f045a3
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-10 20:58:15 -0700

    make blank image gray

--- app/matches/MatchCard.jsx
+++ app/matches/MatchCard.jsx
@@ -13,7 +13,7 @@ const MatchCard = ({ match, onClick }) => {
           alt=""
           width={100} 
           height={100} 
-          className={`rounded-full object-cover ${!match.profileImage && 'blur-sm grayscale'}`}
+          className={`rounded-full object-cover ${!match.profileImage && 'blur-sm grayscale bg-gray-300'}`}
           onError={(e) => { e.target.src = `${apiBasePath}/blank-profile-picture.svg`; }}
         />
         <div className="flex-grow">
