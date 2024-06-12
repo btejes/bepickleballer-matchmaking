c0956c096d6d803686dffad43fb4862ea43bebe5
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-12 00:02:43 -0700

    updating homepage colors and removing big text

--- app/homepage/page.jsx
+++ app/homepage/page.jsx
@@ -8,12 +8,9 @@ export const metadata = {
 
 const Homepage = () => {
   return (
-    <div className="min-h-screen bg-lightGreen flex flex-col">
+    <div className="min-h-screen bg-background flex flex-col">
       <Navbar />
       <div className="flex-grow flex flex-col items-center justify-center px-4">
-        <h1 className="text-2xl md:text-4xl font-bold mb-8 text-center text-black">
-          Looking for a Pickleballer ?
-        </h1>
         <ClientButtons />
       </div>
     </div>
