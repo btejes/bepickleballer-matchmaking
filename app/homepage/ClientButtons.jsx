512456b1ec8ab7d36b9dba3fa2952aa1740dbfe2
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-12 00:13:41 -0700

    updated text for future feature & color in matches

--- app/homepage/ClientButtons.jsx
+++ app/homepage/ClientButtons.jsx
@@ -121,7 +121,7 @@ const ClientButtons = () => {
       {isTournamentModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white rounded-lg p-8 m-4 max-w-xs max-h-full text-center">
-            <h2 className="text-xl font-bold mb-4">Feature Coming Soon!</h2>
+            <h2 className="text-xl font-bold mb-4">Future Feature!</h2>
             <p className="mb-4">
               Tournament matchmaking has not been developed yet. If you have interest in this, email <a href="mailto:ben@bepickleballer.com" className="text-blue-500">ben@bepickleballer.com</a> to show interest in this future feature.
             </p>
