9bca807570b02ff38baf1ed31d204cf309750954
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-12 18:10:31 -0700

    attempt adding logo to brwoser tab

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon-logo.png" />
       </head>
       <body className={`${inter.className} bg-lightGreen`}>
         {children}
