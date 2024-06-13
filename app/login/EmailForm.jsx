8356decb9d18a78dd74524054f41c5e95af58b4f
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-12 17:56:13 -0700

    fix build error from signin page quotes

--- app/login/EmailForm.jsx
+++ app/login/EmailForm.jsx
@@ -78,7 +78,7 @@ const EmailForm = () => {
       {/* Footer with Privacy Policy and Terms of Service */}
       <div className="mt-6 text-center text-black">
         <p className="text-sm">
-          By clicking "Get Magic Link" and signing up, you agree to Be Pickle Baller's 
+          By clicking &quot;Get Magic Link&quot; and signing up, you agree to Be Pickle Baller&apos;s 
           <a href="https://bepickleballer.com/terms-of-service/" className="text-blue-500 hover:underline"> Terms of Service</a> 
           and that you have read our 
           <a href="https://bepickleballer.com/privacy-policy/" className="text-blue-500 hover:underline"> Privacy Policy</a>.
