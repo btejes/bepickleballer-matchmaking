886c842bc68407b62f4e9c14032d5c0867b311ce
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-12 19:53:37 -0700

    incomplete profile text now black and visible

--- app/homepage/ClientButtons.jsx
+++ app/homepage/ClientButtons.jsx
@@ -97,10 +97,10 @@ const ClientButtons = () => {
 
       {isLocalPlayModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white rounded-lg p-8 m-4 max-w-xs max-h-full text-center">
+          <div className="bg-white text-black rounded-lg p-8 m-4 max-w-xs max-h-full text-center">
             <h2 className="text-xl font-bold mb-4">Incomplete Profile</h2>
             <p className="mb-4">
-              To access local matchmaking, you need to complete your profile. Please fill in all the required fields.
+              To access local matchmaking, you need to complete your profile. Please fill in all the required fields on the profile page.
             </p>
             <ul className="list-disc list-inside text-left mb-4">
               {missingFields.map((field) => (
