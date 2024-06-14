4999d0c58ccadbfbc8f8401e4c50094b5a3e5060
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-14 11:40:01 -0700

    updated handling attempt zipcode issue fix

--- library/ProfileForm.jsx
+++ library/ProfileForm.jsx
@@ -38,7 +38,6 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
   const debouncedHandleZipCodeChange = useCallback(debounce(async (zipCode) => {
     const basePath = '/matchmaking';
     if (/^\d{5}$/.test(zipCode)) {
-      console.log("\nDebounced Zipcode:", zipCode, "\n");
       try {
         const response = await fetch(`${basePath}/api/get-city-by-zipcode`, {
           method: 'POST',
@@ -49,13 +48,10 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
           body: JSON.stringify({ zipCode }),
         });
 
-        console.log("\nResponse Status:", response.status, "\n");
-
         if (response.status === 404) {
           setErrors((prevErrors) => ({ ...prevErrors, zipCode: 'City not found for the provided ZIP code' }));
         } else if (response.status === 500) {
           const { error } = await response.json();
-          console.error('Error:', error);
           setErrors((prevErrors) => ({ ...prevErrors, zipCode: 'Internal server error' }));
         } else if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -63,26 +59,26 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
           const data = await response.json();
           if (data.city) {
             setFormData((prevData) => ({ ...prevData, city: data.city }));
-            onProfileChange({ ...formData, city: data.city });
+            onProfileChange((prevProfile) => ({ ...prevProfile, city: data.city }));
           }
         }
       } catch (error) {
-        console.error('Error fetching city:', error);
+        setErrors((prevErrors) => ({ ...prevErrors, zipCode: 'Error fetching city' }));
       }
     }
-  }, 500), [formData]);
+  }, 500), []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     let updatedValue = value;
-    console.log("\nUpdated Value:", updatedValue, "\n");
     let error = '';
 
     if (name === 'zipCode') {
-      setFormData((prevData) => ({ ...prevData, zipCode: updatedValue }));
-      console.log("\nZipcode value:", updatedValue, "\n");
-      if (updatedValue.length === 5) {
-        debouncedHandleZipCodeChange(updatedValue);
+      if (updatedValue.length <= 5) {
+        setFormData((prevData) => ({ ...prevData, zipCode: updatedValue }));
+        if (updatedValue.length === 5) {
+          debouncedHandleZipCodeChange(updatedValue);
+        }
       }
     } else if (name === 'duprRating') {
       if (updatedValue === '' || (/^(2(\.\d{1,2})?|[3-7](\.\d{1,2})?|8(\.0{0,2})?)$/.test(updatedValue))) {
@@ -111,7 +107,7 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
 
     setErrors((prevErrors) => ({ ...prevErrors, [name]: error }));
     setFormData((prevData) => ({ ...prevData, [name]: updatedValue }));
-    onProfileChange({ ...formData, [name]: updatedValue });
+    onProfileChange((prevProfile) => ({ ...prevProfile, [name]: updatedValue }));
   };
 
   const handleKeyPress = (e, name) => {
@@ -225,7 +221,6 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
             <option value="Advanced">Advanced</option>
           </select>
           <label htmlFor="openForMatches">Open For Matches</label>
-         
           <select
             id="openForMatches"
             name="openForMatches"
@@ -263,7 +258,7 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
           </select>
         </div>
       </div>
-    
+
       <div className="grid grid-cols-2 gap-4">
         <div className="col-span-2 flex flex-col relative">
           <div className="relative">
