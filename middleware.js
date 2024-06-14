9661da5477fe31bcde954dfbb647289754626360
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-14 15:45:18 -0700

    Simplify middleware and remove middleware cache

--- middleware.js
+++ middleware.js
@@ -1,17 +1,45 @@
-import { authMiddleware } from './middlewares/auth-middleware';
-import { cacheControlMiddleware } from './middlewares/cache-control-middleware';
+// middleware.js
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { jwtVerify } from 'jose';
 
-export async function middleware(request) {
-  // Apply cache control middleware first
-  let response = cacheControlMiddleware(request);
+export async function authMiddleware(request) {
+  const { pathname } = request.nextUrl;
 
-  // Apply auth middleware
-  response = await authMiddleware(request) || response;
+  console.log(`Middleware activated. Request Path: ${pathname}`);
+  console.log("\nRequest URL:", request.url, "\n");
 
+  // Log all cookies
+  console.log('All cookies:', cookies().getAll());
+
+  // Read JWT token from cookies for protected routes
+  const token = cookies().get('token')?.value;
+
+  console.log(`JWT Token: ${token}`);
+
+  if (!token) {
+    console.log('No JWT Token found. Redirecting to root.');
+    // Uncomment the next line if you want to redirect to the login page when no token is found
+    // return NextResponse.redirect(new URL('/matchmaking', request.url));
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
+    request.user = payload;
+    console.log('Token Verified:', payload);
+
+    const response = NextResponse.next();
+    return response;
+  } catch (error) {
+    console.log('JWT Verification Error:', error);
+    // Uncomment the next line if you want to redirect to the login page on token verification failure
+    // return NextResponse.redirect(new URL('/matchmaking', request.url));
+  }
+
+  const response = NextResponse.next();
   return response;
 }
 
-// Apply middleware to all routes
 export const config = {
-  matcher: '/:path*',
+  matcher: ['/profile', '/homepage', '/matches', '/local-play'],
 };
