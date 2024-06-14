7a01af922fad72d79ef60bd048fbba334cb56a71
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-13 21:48:04 -0700

    moved excel file into library and udpated its api

--- app/api/get-city-by-zipcode/route.js
+++ app/api/get-city-by-zipcode/route.js
@@ -49,7 +49,7 @@ export async function POST(request) {
       return NextResponse.json({ error: 'ZIP code is required' }, { status: 400 });
     }
 
-    const filePath = path.resolve('US_Zipcode_Cities.xlsx'); // Path to your .xlsx file in the root folder
+    const filePath = path.resolve('library', 'US_Zipcode_Cities.xlsx'); // Path to your .xlsx file in the library folder
     console.log(`Using file path: ${filePath}`);
     const city = findCityByZipCode(filePath, zipCode);
 
