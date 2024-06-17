499f3f5a80f36172e6e5620f73853eda3fe2a1fa
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-17 03:12:48 -0700

    update profileCard

--- library/ProfileCard.jsx
+++ library/ProfileCard.jsx
@@ -129,11 +129,11 @@ const ProfileCard = ({ profile, isProfilePage }) => {
 
   return (
     <div className="w-full max-w-xs bg-white border border-gray-300 rounded-3xl shadow-md mx-auto">
-      <div className="relative w-full h-48 object-cover rounded-t-3xl">
+      <div className="relative w-full h-48 rounded-t-3xl overflow-hidden">
         <img
           src={image || `${apiBasePath}/blank-profile-picture.svg`}
           alt="Profile"
-          className={`object-center rounded-t-3xl overflow-hidden ${!isProfilePage && (!image && 'blur-sm grayscale')} ${isProfilePage ? 'cursor-pointer' : ''}`}
+          className={`object-cover w-full h-full rounded-t-3xl ${!isProfilePage && (!image && 'blur-sm grayscale')} ${isProfilePage ? 'cursor-pointer' : ''}`}
           onError={(e) => { e.target.src = `${apiBasePath}/blank-profile-picture.svg`; }}
           onClick={isProfilePage ? () => document.getElementById('imageUpload').click() : null}
         />
