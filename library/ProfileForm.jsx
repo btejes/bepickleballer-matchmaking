b99d38c67737bd40fa183f5847ab599872bf34da
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-17 15:26:33 -0700

    removed top maragin for save button profileform

--- library/ProfileForm.jsx
+++ library/ProfileForm.jsx
@@ -322,7 +322,7 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
 
       {message && (
         <div
-          className={`text-center rounded ${fadeOut ? 'opacity-0 transition-opacity duration-1000' : 'opacity-100'}`}
+          className={`text-center p-2 rounded ${fadeOut ? 'opacity-0 transition-opacity duration-1000' : 'opacity-100'}`}
           style={{
             color: message.startsWith('Error') ? 'red' : 'green',
             transition: 'opacity 1s ease-in-out',
@@ -332,7 +332,7 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave }) => {
         </div>
       )}
 
-      <div className="mt-6 flex justify-center">
+      <div className="flex justify-center">
         <button
           type="submit"
           disabled={!isFormReady || isSaving}
