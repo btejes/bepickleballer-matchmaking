416deed6763f6e0f6a6e6ec81dceea93b1d951da
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-17 03:00:09 -0700

    update to make text better and image better

--- library/ProfileCard.jsx
+++ library/ProfileCard.jsx
@@ -128,8 +128,8 @@ const ProfileCard = ({ profile, isProfilePage }) => {
   };
 
   return (
-    <div className="w-full max-w-xs bg-white border border-gray-300 rounded-3xl shadow-md mx-auto overflow-hidden">
-      <div className="relative w-full h-48 object-contain rounded-t-3xl overflow-hidden">
+    <div className="w-full max-w-xs bg-white border border-gray-300 rounded-3xl shadow-md mx-auto">
+      <div className="relative w-full h-48 object-cover rounded-t-3xl overflow-hidden">
         <img
           src={image || `${apiBasePath}/blank-profile-picture.svg`}
           alt="Profile"
