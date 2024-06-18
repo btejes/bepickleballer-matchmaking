31531ea0c050c72e11b24b5e8e3d64d067c4fe3a
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-17 17:26:28 -0700

    revert back to fixed desktop buttons

--- app/local-play/page.jsx
+++ app/local-play/page.jsx
@@ -132,23 +132,21 @@ const LocalPlay = () => {
       <div className="flex-grow w-full flex flex-col items-center justify-center px-4 space-y-4 lg:space-y-0 lg:flex-row lg:space-x-4">
         {currentMatch ? (
           <div className="flex flex-col items-center justify-center space-y-4 lg:space-y-0 lg:flex-row lg:space-x-4">
-            <div className="flex flex-row space-x-4">
-              <button
-                onClick={() => handleDecision('no')}
-                className="bg-red-500 text-white py-3 px-6 rounded-full w-auto"
-              >
-                No
-              </button>
-              <button
-                onClick={() => handleDecision('yes')}
-                className="bg-green-500 text-white py-3 px-6 rounded-full w-auto"
-              >
-                Yes
-              </button>
-            </div>
+            <button
+              onClick={() => handleDecision('no')}
+              className="bg-red-500 text-white py-3 px-6 rounded-full lg:order-1 order-2 lg:self-center w-auto lg:w-auto"
+            >
+              No
+            </button>
             <div className="order-1 lg:order-2 flex justify-center w-full lg:w-auto">
               <ProfileCard profile={currentMatch} />
             </div>
+            <button
+              onClick={() => handleDecision('yes')}
+              className="bg-green-500 text-white py-3 px-6 rounded-full lg:order-3 order-2 lg:self-center w-auto lg:w-auto"
+            >
+              Yes
+            </button>
           </div>
         ) : (
           <p className="text-center">
