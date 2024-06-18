b251bbcb1a9a9ca32033ff533de44bab3e55a5db
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-17 20:14:10 -0700

    updated matches card for mobile friendly

--- app/matches/MatchCard.jsx
+++ app/matches/MatchCard.jsx
@@ -4,20 +4,20 @@ import React from 'react';
 const MatchCard = ({ match, onClick }) => {
   console.log("Match profile image URL:", match.profileImage); 
   return (
-    <div className="bg-white border text-black  border-gray-300 rounded-3xl p-4 shadow-md w-full max-w-2xl mb-4 cursor-pointer" onClick={onClick}>
-      <div className="flex items-center justify-between">
+    <div className="bg-white border text-black border-gray-300 rounded-3xl p-4 shadow-md w-full max-w-2xl mb-4 cursor-pointer" onClick={onClick}>
+      <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
         <div className="w-24 h-24 rounded-full overflow-hidden mr-5">
           <img 
             src={match.profileImage} 
             alt=""
-            className={`w-full h-full  object-cover aspect-ratio ${!match.profileImage && 'blur-sm grayscale bg-gray-300'}`}
+            className={`w-full h-full object-cover ${!match.profileImage && 'blur-sm grayscale bg-gray-300'}`}
             onError={(e) => { e.target.src = `${apiBasePath}/blank-profile-picture.svg`; }}
           />
         </div>
-        <div className="flex-grow">
+        <div className="flex-grow text-center md:text-left">
           <h2 className="text-lg font-semibold">{match.firstName}</h2>
         </div>
-        <div className="flex space-x-6">
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 text-center md:text-left">
           <p className="text-black">DUPR: {match.duprRating}</p>
           <p className="text-black">Age: {match.ageRange}</p>
           <p className="text-black">Level: {match.skillLevel}</p>
