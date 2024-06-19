025453f263ab4681ad5016e688d33039baba2c4d
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-18 18:44:28 -0700

    update navbar to use smaller logo on mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
@@ -6,44 +7,63 @@ const Navbar = () => {
     <nav className="navbar bg-white px-2 py-1 shadow-md flex justify-between items-center">
       <div className="flex items-center">
         <Link href="https://bepickleballer.com">
-          <img
-            src={`${basePath}/logo.png`}
-            alt="Logo"
-            className="h-10 w-auto sm:h-16 sm:w-auto lg:h-20 lg:w-auto cursor-pointer mr-2"
-          />
+          <a>
+            <Image
+              src={`${basePath}/favicon-logo.png`}
+              alt="Mobile Logo"
+              className="h-8 w-8 sm:hidden cursor-pointer mr-2"
+              width={32}
+              height={32}
+            />
+            <Image
+              src={`${basePath}/logo.png`}
+              alt="Desktop Logo"
+              className="hidden sm:block h-10 w-auto sm:h-16 sm:w-auto lg:h-20 lg:w-auto cursor-pointer mr-2"
+              width={100}
+              height={100}
+            />
+          </a>
         </Link>
       </div>
       <div className="flex items-center space-x-2 sm:space-x-4">
-        <Link href="/homepage" className="text-black hover:underline text-sm sm:text-base">
-          <span className="block lg:hidden">Find</span>
-          <span className="hidden lg:block">Find Match</span>
+        <Link href="/homepage">
+          <a className="text-black hover:underline text-sm sm:text-base">
+            <span className="block lg:hidden">Find</span>
+            <span className="hidden lg:block">Find Match</span>
+          </a>
         </Link>
-        <Link href="/matches" className="text-black hover:underline text-sm sm:text-base">
-          <span className="block lg:hidden">Matches</span>
-          <span className="hidden lg:block">My Matches</span>
+        <Link href="/matches">
+          <a className="text-black hover:underline text-sm sm:text-base">
+            <span className="block lg:hidden">Matches</span>
+            <span className="hidden lg:block">My Matches</span>
+          </a>
         </Link>
-        <Link href="/faq" className="text-black hover:underline">
-          <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black">
-            ?
-          </div>
+        <Link href="/faq">
+          <a className="text-black hover:underline">
+            <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black">
+              ?
+            </div>
+          </a>
         </Link>
-        <Link href="/profile" className="text-black hover:underline">
-          <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-4 h-4 sm:w-6 sm:h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM16 14H8c-2.21 0-4 1.79-4 4v1h16v-1c0-2.21-1.79-4-4-4z"
-              />
-            </svg>
-          </div>
+        <Link href="/profile">
+          <a className="text-black hover:underline">
+            <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-4 h-4 sm:w-6 sm:h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM16 14H8c-2.21 0-4 1.79-4 4v1h16v-1c0-2.21-1.79-4-4-4z"
+                />
+              </svg>
+            </div>
+          </a>
         </Link>
       </div>
     </nav>
