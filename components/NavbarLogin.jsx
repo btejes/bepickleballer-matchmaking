18e1d16330ed14c136bb2ff0690b6691820dd6dc
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-20 16:34:33 -0700

    trying width fix for login navbar

--- components/NavbarLogin.jsx
+++ components/NavbarLogin.jsx
@@ -8,16 +8,18 @@ const NavbarBasicLogo = () => {
     <nav className="w-full bg-white px-4 py-2 shadow-md flex justify-between items-center">
       <div className="flex items-center">
         <Link href="https://bepickleballer.com">
-          <Image
-            src={`${basePath}/logo.png`}
-            alt="Desktop Logo"
-            className="cursor-pointer mr-2"
-            style={{ maxWidth: '400px', height: 'auto' }}
-            width={634}
-            height={117}
-            quality={100}
-            priority
-          />
+          <div className="max-w-full sm:max-w-[400px]">
+            <Image
+              src={`${basePath}/logo.png`}
+              alt="Desktop Logo"
+              className="cursor-pointer mr-2"
+              style={{ width: '100%', height: 'auto' }}
+              width={634}
+              height={117}
+              quality={100}
+              priority
+            />
+          </div>
         </Link>
       </div>
       <div className="flex items-center space-x-4">
