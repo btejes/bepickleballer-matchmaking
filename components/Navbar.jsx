0d69b4cb989ed6ef40d42c06b4f8be76a587a6bb
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-20 15:30:01 -0700

    update to fix minified errors hopefully

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,76 +3,61 @@ import Image from 'next/image';
 
 const Navbar = () => {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
   return (
     <nav className="navbar bg-white px-2 sm:p-4 py-1 shadow-md flex justify-between items-center">
       <div className="flex items-center">
         <Link href="https://bepickleballer.com">
-          <a>
-            <div className="h-8 w-8 sm:hidden cursor-pointer mr-2 relative">
-              <Image
-                src={`${basePath}/favicon-logo.png`}
-                alt="Mobile Logo"
-                layout="responsive"
-                width={32}
-                height={32}
-                quality={100}
-                priority
-              />
-            </div>
-            <div className="hidden sm:block cursor-pointer mr-2 relative" style={{ maxWidth: '200px', width: '100%' }}>
-              <Image
-                src={`${basePath}/logo.png`}
-                alt="Desktop Logo"
-                layout="responsive"
-                width={634}
-                height={117}
-                quality={100}
-                priority
-              />
-            </div>
-          </a>
+          <Image
+            src={`${basePath}/favicon-logo.png`}
+            alt="Mobile Logo"
+            className="h-8 w-8 sm:hidden cursor-pointer mr-2"
+            width={32}
+            height={32}
+            quality={100}
+            priority
+          />
+          <Image
+            src={`${basePath}/logo.png`}
+            alt="Desktop Logo"
+            className="hidden sm:block h-10 sm:h-16 lg:h-20 cursor-pointer mr-2"
+            width={634}
+            height={117}
+            quality={100}
+            priority
+          />
         </Link>
       </div>
       <div className="flex items-center space-x-4 sm:space-x-6">
-        <Link href="/findmatch">
-          <a className="text-black hover:underline text-base sm:text-lg">
-            <span className="block lg:hidden">Find</span>
-            <span className="hidden lg:block">Find Match</span>
-          </a>
+        <Link href="/findmatch" className="text-black hover:underline text-base sm:text-lg">
+          <span className="block lg:hidden">Find</span>
+          <span className="hidden lg:block">Find Match</span>
         </Link>
-        <Link href="/matches">
-          <a className="text-black hover:underline text-base sm:text-lg">
-            <span className="block lg:hidden">Matches</span>
-            <span className="hidden lg:block">My Matches</span>
-          </a>
+        <Link href="/matches" className="text-black hover:underline text-base sm:text-lg">
+          <span className="block lg:hidden">Matches</span>
+          <span className="hidden lg:block">My Matches</span>
         </Link>
-        <Link href="/faq">
-          <a className="text-black hover:underline">
-            <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black text-lg">
-              ?
-            </div>
-          </a>
+        <Link href="/faq" className="text-black hover:underline">
+          <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black text-lg">
+            ?
+          </div>
         </Link>
-        <Link href="/profile">
-          <a className="text-black hover:underline">
-            <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5 sm:w-7 sm:h-7"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM16 14H8c-2.21 0-4 1.79-4 4v1h16v-1c0-2.21-1.79-4-4-4z"
-                />
-              </svg>
-            </div>
-          </a>
+        <Link href="/profile" className="text-black hover:underline">
+          <div className="flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 rounded-full border border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-5 h-5 sm:w-7 sm:h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM16 14H8c-2.21 0-4 1.79-4 4v1h16v-1c0-2.21-1.79-4-4-4z"
+              />
+            </svg>
+          </div>
         </Link>
       </div>
     </nav>
--- components/NavbarLogin.jsx
+++ components/NavbarLogin.jsx
@@ -8,20 +8,17 @@ const NavbarBasicLogo = () => {
     <nav className="w-full bg-white px-4 py-2 shadow-md flex justify-between items-center">
       <div className="flex items-center">
         <Link href="https://bepickleballer.com">
-          <a>
-            <div className="relative" style={{ maxWidth: '200px', width: '100%' }}>
-              <Image
-                src={`${basePath}/logo.png`}
-                alt="Logo"
-                layout="responsive"
-                width={634}
-                height={117}
-                quality={100}
-                priority
-                className="cursor-pointer"
-              />
-            </div>
-          </a>
+          <div className="relative" style={{ maxWidth: '200px', width: '100%' }}>
+            <Image
+              src={`${basePath}/logo.png`}
+              alt="Logo"
+              width={634}
+              height={117}
+              quality={100}
+              priority
+              className="cursor-pointer"
+            />
+          </div>
         </Link>
       </div>
       <div className="flex items-center space-x-4">
