e4418d25ac44fc1f32681ed0474faed59feea7ef
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-20 23:40:25 -0700

    test if issue is with new Image or not

--- app/matches/MatchCard.jsx
+++ app/matches/MatchCard.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
 
 const MatchCard = ({ match, onClick }) => {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-  const [imgSrc, setImgSrc] = useState(match.profileImage || `${basePath}/blank-profile-picture.svg`);
+  const defaultImgSrc = `${basePath}/blank-profile-picture.svg`;
+  const [imgSrc, setImgSrc] = useState(match.profileImage || defaultImgSrc);
+
+  useEffect(() => {
+    console.log("Match profile image URL:", match.profileImage);
+    console.log("Using image source:", imgSrc);
+  }, [imgSrc, match.profileImage]);
 
   const handleError = () => {
-    setImgSrc(`${basePath}/blank-profile-picture.svg`);
+    console.log("Image failed to load, using fallback image.");
+    setImgSrc(defaultImgSrc);
   };
 
   return (
     <div className="bg-white border text-black border-gray-300 rounded-3xl p-4 shadow-md w-full max-w-2xl mb-4 cursor-pointer" onClick={onClick}>
       <div className="flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
         <div className="w-24 h-24 rounded-full overflow-hidden mr-5">
-          <Image 
-            src={imgSrc} 
-            alt=""
+          <img 
+            src={imgSrc}
+            alt="Profile Image"
             width={96}
             height={96}
             className={`w-full h-full object-cover ${!match.profileImage && 'blur-sm grayscale bg-gray-300'}`}
