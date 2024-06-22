c75d18fcc6ae9601806dfe3cad3e447a6dc8ecfb
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-21 19:41:37 -0700

    try to fix width

--- library/ProfileForm.jsx
+++ library/ProfileForm.jsx
@@ -141,8 +141,8 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave, isUploading }) =
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-white p-4 rounded-3xl shadow-md text-black max-w-2xl mx-auto">
-        <div className="flex flex-row box-border">
+    <form onSubmit={handleSubmit} className="bg-white p-4 rounded-3xl shadow-md text-black max-w-2xl mx-auto box-border">
+        <div className="flex box-border">
             <div className="w-1/2 p-2 box-border">
                 <label htmlFor="firstName">First Name</label>
                 <input
@@ -361,6 +361,7 @@ const ProfileForm = ({ profile, onProfileChange, onProfileSave, isUploading }) =
             </button>
         </div>
     </form>
+
 
 
 
