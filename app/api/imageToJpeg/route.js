4b68b38155a42a3c94bd933149644c31820d0c21
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-06-22 20:39:23 -0700

    update heic stop try get width height

--- app/api/imageToJpeg/route.js
+++ app/api/imageToJpeg/route.js
@@ -8,14 +8,14 @@ import path from 'path';
 
 ffmpeg.setFfmpegPath('ffmpeg');
 
-async function processImage(file, userId) {
-  console.log(`Processing image for user: ${userId}`);
+async function processHEICImage(file, userId) {
+  console.log("Processing HEIC image");
 
   const imageBuffer = Buffer.from(file.buffer, 'base64');
   console.log(`Image buffer size: ${imageBuffer.length} bytes`);
 
   const tempDir = os.tmpdir();
-  const inputPath = path.join(tempDir, `input_${Date.now()}.${file.type.split('/')[1]}`);
+  const inputPath = path.join(tempDir, `input_${Date.now()}.heic`);
   const outputPath = path.join(tempDir, `output_${Date.now()}.jpg`);
   console.log(`Input path: ${inputPath}`);
   console.log(`Output path: ${outputPath}`);
@@ -39,7 +39,7 @@ async function processImage(file, userId) {
           console.log('Processing: ' + progress.percent + '% done');
         })
         .on('end', () => {
-          console.log('Image conversion completed');
+          console.log('HEIC to JPEG conversion completed');
           resolve();
         })
         .on('error', (err, stdout, stderr) => {
@@ -59,42 +59,11 @@ async function processImage(file, userId) {
     await fs.unlink(inputPath);
     await fs.unlink(outputPath);
 
-    console.log("Image processing completed successfully");
+    console.log("HEIC image processing completed successfully");
     return NextResponse.json({ image: base64Image }, { status: 200 });
   } catch (error) {
-    console.error('Error during image processing:', error);
-    return NextResponse.json({ error: 'Failed to process image', details: error.message }, { status: 500 });
-  }
-}
-
-export async function POST(req) {
-  try {
-    const jwtToken = cookies().get('token')?.value;
-
-    if (!jwtToken) {
-      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-    }
-
-    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
-    const userId = decoded._id;
-
-    const body = await req.json();
-    const { file } = body;
-
-    if (!file) {
-      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
-    }
-
-    console.log(`File type: ${file.type}`);
-    const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp', 'image/heic'];
-    if (!allowedTypes.includes(file.type)) {
-      return NextResponse.json({ error: `${file.type} not accepted. Please submit one of the following: JPEG, JPG, PNG, WEBP, HEIC` }, { status: 400 });
-    }
-
-    return await processImage(file, userId);
-  } catch (error) {
-    console.error('Error processing the image upload:', error);
-    return NextResponse.json({ error: 'Failed to process image' }, { status: 500 });
+    console.error('Error during HEIC processing:', error);
+    return NextResponse.json({ error: 'Failed to process HEIC image', details: error.message }, { status: 500 });
   }
 }
 
